941e538d9dacfb3e93125d95db21b5d7f36bc140
Andrew Casper <acasper@google.com>
2020-07-19 18:11:15 -0400

    Update TCL files to allow multiple LEF files defining the SCs.

--- flow/scripts/pdn.tcl
+++ flow/scripts/pdn.tcl
@@ -1,7 +1,9 @@
 if {![info exists standalone] || $standalone} {
   # Read lef
   read_lef $::env(TECH_LEF)
-  read_lef $::env(SC_LEF)
+  foreach lef $::env(SC_LEF)
+    read_lef $lef
+  end
   if {[info exist ::env(ADDITIONAL_LEFS)]} {
     foreach lef $::env(ADDITIONAL_LEFS) {
       read_lef $lef
